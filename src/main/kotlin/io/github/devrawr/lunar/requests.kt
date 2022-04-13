fce307d88d1c3e1f0e99227bef0c73137f6732b9
GrowlyX <growlyxdev@gmail.com>
2022-04-12 21:12:00 -0400

    todo dev

--- src/main/kotlin/io/github/devrawr/lunar/requests.kt
+++ src/main/kotlin/io/github/devrawr/lunar/requests.kt
@@ -2,6 +2,7 @@ package io.github.devrawr.lunar
 
 import java.net.URL
 
+// TODO: 4/12/2022 allow lunarclientdev.com as well
 val url = URL("https://api.lunarclientprod.com/game/metadata")
 
 fun readLunarMetadata(): String
