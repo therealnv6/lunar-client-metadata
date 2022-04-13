befcedd62996ef8b1dca83c4c9a85257117c360e
GrowlyX <growlyxdev@gmail.com>
2022-04-12 22:44:22 -0400

    Sentry?

--- src/main/kotlin/io/github/devrawr/lunar/model/sentry/SentryFilteredException.kt
+++ src/main/kotlin/io/github/devrawr/lunar/model/sentry/SentryFilteredException.kt
@@ -3,6 +3,9 @@ package io.github.devrawr.lunar.model.sentry
 import kotlinx.serialization.Serializable
 
 /**
+ * I really have no idea who will
+ * ever need this, but added it anyway!
+ *
  * @author GrowlyX
  * @since 4/12/2022
  */
