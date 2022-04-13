99a2ed0ed87a4acdde6a4f80c7fabcd207348306
devrawr <rawrdev@protonmail.com>
2022-04-13 04:32:36 +0200

    use same URL object for every call to avoid having to create a new URL object

--- src/main/kotlin/io/github/devrawr/lunar/LunarClientMetadataRetriever.kt
+++ src/main/kotlin/io/github/devrawr/lunar/LunarClientMetadataRetriever.kt
@@ -7,23 +7,33 @@ import java.net.URL
 
 object LunarClientMetadataRetriever
 {
+    /**
+     * Read the metadata from the [LUNAR_METADATA_ENDPOINTS] endpoint.
+     *
+     * This method simply reads from the endpoint and returns
+     * it as a string.
+     *
+     * Will automatically retrieve the correct endpoint
+     * according to the [LunarClientMetadataContext] provided.
+     *
+     * @param context the context used to identify the correct endpoint
+     * @return the data read from the endpoint
+     */
     @JvmStatic
     fun readMetadataAsText(
         context: LunarClientMetadataContext
     ): String
     {
-        val url = URL(
-            String.format(
-                LUNAR_METADATA_ENDPOINT,
-                context.lowercase
+        val url = LUNAR_METADATA_ENDPOINTS[context]
+            ?: throw IllegalArgumentException(
+                "No endpoint found by type of $context"
             )
-        )
 
         return url.readText()
     }
 
     /**
-     * Read the metadata from the [LUNAR_METADATA_ENDPOINT] endpoint.
+     * Read the metadata from the [LUNAR_METADATA_ENDPOINTS] endpoint.
      *
      * This method will automatically try to parse the
      * retrieved data from the server using the provided
--- src/main/kotlin/io/github/devrawr/lunar/constants.kt
+++ src/main/kotlin/io/github/devrawr/lunar/constants.kt
@@ -1,8 +1,14 @@
 package io.github.devrawr.lunar
 
-/**
- * @author GrowlyX
- * @since 4/12/2022
- */
-internal const val LUNAR_METADATA_ENDPOINT =
-    "https://api.lunarclient%s.com/game/metadata"
+import io.github.devrawr.lunar.model.LunarClientMetadataContext
+import java.net.URL
+
+val LUNAR_METADATA_ENDPOINTS = mapOf(
+    LunarClientMetadataContext.Prod to "prod".toLunarEndpoint(),
+    LunarClientMetadataContext.Dev to "dev".toLunarEndpoint()
+)
+
+fun String.toLunarEndpoint(): URL
+{
+    return URL("https://api.lunarclient${this}.com/game/metadata")
+}
