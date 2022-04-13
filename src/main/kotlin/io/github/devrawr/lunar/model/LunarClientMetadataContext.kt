a8b9042e15f57faceb41209706dab70023cc5c9f
GrowlyX <growlyxdev@gmail.com>
2022-04-12 22:41:38 -0400

    Store URl lazily

--- src/main/kotlin/io/github/devrawr/lunar/model/LunarClientMetadataContext.kt
+++ src/main/kotlin/io/github/devrawr/lunar/model/LunarClientMetadataContext.kt
@@ -9,14 +9,11 @@ import java.net.URL
  * @author GrowlyX
  * @since 4/12/2022
  */
-enum class LunarClientMetadataContext(val endpoint: URL)
+enum class LunarClientMetadataContext
 {
-    Prod("prod".lunarEndpoint),
-    Dev("dev".lunarEndpoint);
-}
+    Prod, Dev;
 
-private val String.lunarEndpoint: URL
-    get()
-    {
-        return URL("https://api.lunarclient${this.lowercase()}.com/game/metadata")
+    val endpoint by lazy {
+        URL("https://api.lunarclient${name.lowercase()}.com/game/metadata")
     }
+}
